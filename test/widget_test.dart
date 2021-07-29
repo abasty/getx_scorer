6948c720658966e549009d29f3bfec7ac89247ed
Alain Basty <alain.basty@free.fr>
2021-07-29 08:23:28 +0200

    Added initial checks

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:getx_scorer/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Snackbar test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ScorerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Verify that just one 'Classement' widget is on the screen
+    expect(find.text('Classement'), findsOneWidget);
+    // Tap the 'Classement' button.
+    await tester.tap(find.text('Classement'));
+    // Schedule animation
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Start animation
+    await tester.pump();
+    // Verify that our snackbar is displayed.
+    expect(find.text('Classement'), findsNWidgets(2));
+    // Wait default animation time
+    await tester.pump(const Duration(seconds: 4));
+    // Verify that just one 'Classement' widget is on the screen
+    expect(find.text('Classement'), findsOneWidget);
   });
 }
