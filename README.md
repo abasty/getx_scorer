4a37383060a42392a425c67b585ff5d74375cebc
Alain Basty <alain.basty@free.fr>
2021-07-29 06:09:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# getx_scorer
+
+Une application pour compter les points en Flutter/GetX.
+
+## Getting Started
---
+++ android/app/src/main/kotlin/com/example/getx_scorer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_scorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
