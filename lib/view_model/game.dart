4c7c12efdc7d2c8720e4f0d0775f92920ffab21c
Alain Basty <alain.basty@free.fr>
2021-07-31 08:06:37 +0200

    Added Game class

---
+++ lib/view_model/game.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:localstorage/localstorage.dart';
+
+class Game {
+  final String ID;
+
+  final List<String> players;
+
+  final List<List<int>> table;
+  final List<int> _cancelList = [];
+
+  Game(this.ID, this.players) : table = players.map((e) => <int>[]).toList();
+
+  get cancelable => _cancelList.length > 0;
+
+  get count => max(table[0].length, table[1].length);
+
+  List<int> getScore(int player, int line) {
+    var length = table[player].length;
+    if (line >= length) return [-1, 0];
+    return [
+      table[player][line],
+      line == 0
+          ? table[player][line]
+          : table[player][line] - table[player][line - 1]
+    ];
+  }
+
+  final _storage = LocalStorage('points.json');
+
+  Future<void> readAll() async {
+    await _storage.ready;
+    var map = json.decode(await _storage.getItem('modele') as String);
+    List<List<int>> tmp;
+    table.clear();
+    if (map is List) {
+      tmp = map
+          .map<List<int>>((j) =>
+              j is List ? j.map<int>((p) => p is int ? p : 0).toList() : [])
+          .toList();
+    } else {
+      tmp = players.map((e) => <int>[]).toList();
+    }
+    table.addAll(tmp);
+  }
+
+  Future<void> writeAll() async {
+    await _storage.ready;
+    await _storage.setItem('modele', json.encode(table));
+  }
+
+  void ctrlAddScore(int player, int score) {
+    _cancelList.add(player);
+    var list = table[player];
+    var length = list.length;
+    list.add(length == 0 ? score : list[length - 1] + score);
+    // writeAll();
+  }
+
+  void ctrlCancel() {
+    if (_cancelList.length == 0) return;
+    var player = _cancelList.removeLast();
+    table[player].removeLast();
+    // writeAll();
+  }
+
+  void ctrlRAZ() {
+    table.clear();
+    _cancelList.clear();
+  }
+}
---
+++ test/game_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:getx_scorer/view_model/game.dart';
+
+void main() async {
+  var game = Game('ID1', ['toto', 'tata', 'titi']);
+  test('Game()', () {
+    assert(listEquals(game.getScore(0, 0), [-1, 0]));
+  });
+  test('Game.ctrlAddScore,', () {
+    game.ctrlAddScore(0, 100);
+    game.ctrlAddScore(1, 25);
+    game.ctrlAddScore(2, 75);
+    var sum = 0;
+    for (int i = 0; i < game.players.length; i++) {
+      sum += game.getScore(i, 0)[1];
+    }
+    assert(sum == 200);
+  });
+}
